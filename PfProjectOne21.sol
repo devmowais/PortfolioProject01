d91005ceca09cbf6f46a5f04f0f8645b8baf9b95
Muhammad Owais Sherwani <51533702+devmowais@users.noreply.github.com>
2022-09-16 23:49:31 +0500

    contract num changes

--- PfProjectOne21.sol
+++ PfProjectOne21.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.8.0 <0.9.0;
 
-contract PfProjectOne11 {
+contract PfProjectOne21 {
 address owner;
 
  string public constant name = "Bugs Bunny Token"; //Tokens Name
